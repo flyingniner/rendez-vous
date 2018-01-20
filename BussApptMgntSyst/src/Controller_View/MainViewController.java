9e7f5ce512bbec849c3c372a3700c3cdcf4f2985
Chip <Chip@10.0.1.29>
2018-01-19 22:40:54 -0800

    Created MainView and MainViewController

--- BussApptMgntSyst/src/Controller_View/MainViewController.java
+++ BussApptMgntSyst/src/Controller_View/MainViewController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bussapptmgntsyst;
+package Controller_View;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,7 +16,7 @@ import javafx.scene.control.Label;
  *
  * @author Chip
  */
-public class FXMLDocumentController implements Initializable
+public class MainViewController implements Initializable
 {
     
     @FXML
