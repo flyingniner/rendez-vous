0b44e66f38e33081c4afc5d3e2d4d67ae863a797
Chip <Chip@10.0.1.29>
2018-01-19 21:37:20 -0800

    checking git repo connnection working

--- BussApptMgntSyst/src/bussapptmgntsyst/BussApptMgntSyst.java
+++ BussApptMgntSyst/src/bussapptmgntsyst/BussApptMgntSyst.java
@@ -22,7 +22,7 @@ public class BussApptMgntSyst extends Application
     public void start(Stage stage) throws Exception
     {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+        String hello;
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
